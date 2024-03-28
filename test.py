f5b3ae0bd8a082cff403c63564fcbd682042c68d
ss243 <siddhantsen05@gmail.com>
2024-03-28 16:23:26 -0500

    test file added

---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+The test file will contain sample inputs for the quiz file. 
+It will call the functions in the same order that the final production will. 
+It will also include the same returned print statements with the information for the user. 
+
+"""
