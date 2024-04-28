8d07a31024f18e48ed92682287f0da5aef4abd7c
Siddhant Sen <ssen23@wirelessprv-10-194-112-144.near.illinois.edu>
2024-04-28 16:25:21 -0500

    added instructions file

--- quiz.py
+++ quiz.py
@@ -7,6 +7,7 @@ def do_quiz():
         goal = input("What is the goal of this workout?\n\ta. Get stronger\n\tb. Lose weight\n\tc. Athleticism\n\td. Overall Health\n")
         if (goal in acceptable):
             response_validity = True
+        print("************************\nInvalid input. Try again\n************************")
 
     ## question 2
     response_validity = False
@@ -16,6 +17,8 @@ def do_quiz():
         time = input("How much time can you commit to the workout?\n\ta. <30 min\n\tb. 30-60 min\n\tc. 1-2 hours\n\td. >2 hours\n")
         if (time in acceptable):
             response_validity = True
+        print("************************\nInvalid input. Try again\n************************")
+        
 
     ## question 3
     response_validity = False
@@ -25,6 +28,7 @@ def do_quiz():
         primary_muscle = input("What primary muscle group do you want to target?\n\ta. Chest\n\tb. Back\n\tc. Legs\n\td. Arms\n")
         if (primary_muscle in acceptable):
             response_validity = True
+        print("************************\nInvalid input. Try again\n************************")
 
     ## question 4
     response_validity = False
@@ -34,7 +38,7 @@ def do_quiz():
         secondary_muscle = input("What secondary muscle group do you want to target\n\ta. Chest\n\tb. Back\n\tc. Legs\n\td. Arms\n")
         if (secondary_muscle in acceptable):
             response_validity = True
-
+        print("************************\nInvalid input. Try again\n************************")
 
     ## question 5
     response_validity = False
@@ -44,6 +48,7 @@ def do_quiz():
         experience = input("What is your experience level with working out?\n\ta. Beginner\n\tb. Intermediate\n\tc. Advanced\n")
         if (experience in acceptable):
             response_validity = True
+        print("************************\nInvalid input. Try again\n************************")
         
 
     
