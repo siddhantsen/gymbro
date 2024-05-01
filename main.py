6399795ae918765c2c1fd0452e324833961edf57
Siddhant Sen <ssen23@wirelessprv-10-194-112-144.near.illinois.edu>
2024-05-01 00:22:48 -0500

    added more instructions

--- main.py
+++ main.py
@@ -2,10 +2,55 @@ import quiz, exercise, weight_loss_plan
 from quiz import *
 from exercise import *
 from weight_loss_plan import *
+import time
 
+
+def startSequence():
+    print("You will be prompted with a series of questions. To answer, simply type your response and press enter.")
+    time.sleep(2)
+    print("For multiple choice questions, simply type the lowercase letter response and press enter.")
+    time.sleep(2)
+    print("In multiple choice questions, only the answer choices listed will be accepted. If there are a-d choices, 'e' will not be accepted.")
+    time.sleep(2)
+    print("For numbered answer questions, which will be indicated, simply type your integer answer and press enter.")
+    time.sleep(2)
+    print("Before beginning, we will practice these instructions.")
+    time.sleep(2)
+
+    ## practice question 1
+    response_validity = False
+
+    while (response_validity == False):
+        acceptable = ['a', 'b', 'c', 'd']
+        multi_choice_practice = input("What color is grass?\n\ta. blue\n\tb. green\n\tc. red\n\td. purple\n")
+        if (multi_choice_practice in acceptable):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+
+    ## practice question 2
+    response_validity = False
+
+    while (response_validity == False):
+        
+        number_practice = int(input("Enter a number between 1 and 10,000:\n"))
+
+        if (number_practice > 0 and number_practice <= 10000):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+
+    print("Great job! You've got the hang of it! We'll begin now. Enjoy!")
+    
+
+    
+    
+
+    
 
 
 def startDecision():
+    
     decision = input("Would you like to plan a workout or get weight loss tips.\n\ta. workout\n\tb. weight loss\n")
     return True, decision
 
@@ -16,6 +61,7 @@ def endDecision():
     else:
         return False
 def main():
+    startSequence()
     go = True
     while (go == True):
         go, decision = startDecision()
