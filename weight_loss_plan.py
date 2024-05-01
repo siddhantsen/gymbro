2a64aff2e88e6f3e96738176a1251c1a025827c6
Siddhant Sen <ssen23@wirelessprv-10-194-112-144.near.illinois.edu>
2024-05-01 00:11:53 -0500

    made weight loss quiz check for invalid inputs

--- weight_loss_plan.py
+++ weight_loss_plan.py
@@ -1,10 +1,75 @@
 def weight_quiz():
-    current_weight = int(input("What is your current weight in pounds:\n"))
-    height = int(input("What is your height in inches:\n"))
-    age = int(input("What is your age:\n"))
-    exercise_level = input("What is your exercise level:\n\ta. sedentary\n\tb. lightly active (exercise 2-3 times per week)\n\tc. moderately active (exercise 4-5 times per week)\n\td. very active (exercise 6-7 times per week)")
-    goal_weight = int(input("What is your goal weight in pounds: \n"))
-    timeline = int(input("In how many weeks do you want to lose this weight:\n"))
+    ## question 1
+    response_validity = False
+    while (response_validity == False):
+        
+        current_weight = int(input("What is your current weight in pounds:\n"))
+        if (current_weight > 0 and current_weight < 10000):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+
+    ## question 2
+    response_validity = False
+
+    while (response_validity == False):
+        acceptable = ['a', 'b', 'c', 'd']
+        height = int(input("What is your height in inches:\n"))
+        if (height > 0 and height < 10000):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+        
+
+    ## question 3
+    response_validity = False
+
+    while (response_validity == False):
+        
+        age = int(input("What is your age:\n"))
+        if (age > 0 and age < 10000):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+
+    ## question 4
+    response_validity = False
+
+    while (response_validity == False):
+        acceptable = ['a', 'b', 'c', 'd']
+        exercise_level = input("What is your exercise level:\n\ta. sedentary\n\tb. lightly active (exercise 2-3 times per week)\n\tc. moderately active (exercise 4-5 times per week)\n\td. very active (exercise 6-7 times per week)\n")
+        if (exercise_level in acceptable):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+
+    ## question 5
+    response_validity = False
+
+    while (response_validity == False):
+        acceptable = ['a', 'b', 'c']
+        goal_weight = int(input("What is your goal weight in pounds: \n"))
+        if (goal_weight > 0 and goal_weight < current_weight):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+    
+
+    ## question 6
+    response_validity = False
+
+    while (response_validity == False):
+        
+        timeline = int(input("In how many weeks do you want to lose this weight:\n"))
+        if (timeline > 0 and timeline < 10000):
+            response_validity = True
+            break
+        print("************************\nInvalid input. Try again\n************************")
+    
+    
+    
+    
+    
     response_tuple = (current_weight, height, age, exercise_level, goal_weight, timeline)
     return response_tuple
 
@@ -40,6 +105,10 @@ def generate_plan(response_tuple):
     
 
     
+
+sample_tuple = (125, 63, 19, "c", 119, 4)
+print(get_basal_metabolic_rate(sample_tuple))
+generate_plan(sample_tuple)
 
 
 
